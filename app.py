c111ef272a3a9c37f7a232ea5c58b05226d4bc2e
MaQsood Shah <mr.maqsood.shah@gmail.com>
2022-02-28 12:17:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import time
 
-def print_name(email: str)-> str:
+# 
+def print_email(email: str)-> str:
     time.sleep(2)
     return email
 
-print('Hello Everyone! This is Openshift cluster deployment test.\n', print_name("Contact: mr.maqsood.shah@gmail.com"))
+print('Hello Everyone! This is Openshift cluster deployment test.\n', print_email("Contact: mr.maqsood.shah@gmail.com"))

ed7e80bd0ddcdc7c0b15a547a3eefb1df439bd01
MaQsood Shah <mr.maqsood.shah@gmail.com>
2022-03-01 12:21:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     time.sleep(2)
-    return "Hello Everyone! This is Openshift cluster deployment test.\n Contact: mr.maqsood.shah@gmail.com"
+    return "Hello Everyone!I'm MaQs. This is Openshift cluster deployment test.\n Contact: mr.maqsood.shah@gmail.com"
   
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
